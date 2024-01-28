219b9c60521d78ff7b2b4a02b5257dbc5bd086ae
Jason Hall <jason@chainguard.dev>
2024-01-28 16:15:20 -0500

    async boilerplate
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- index.js
+++ index.js
@@ -14,18 +14,21 @@ if (!scope || !identity) {
     process.exit(1);
 }
 
-try {
-    const res = await fetch(`${actionsUrl}&audience=octo-sts.dev`, { headers: { 'Authorization': `Bearer ${actionsToken}` } });
-    const json = await res.json();
-    const res2 = await fetch(`https://octo-sts.dev/sts/exchange?scope=${scope}&identity=${identity}`, { headers: { 'Authorization': `Bearer ${json.value}` } });
-    const json2 = await res2.json();
+(async function main() {
+    // You can use await inside this function block
+    try {
+        const res = await fetch(`${actionsUrl}&audience=octo-sts.dev`, { headers: { 'Authorization': `Bearer ${actionsToken}` } });
+        const json = await res.json();
+        const res2 = await fetch(`https://octo-sts.dev/sts/exchange?scope=${scope}&identity=${identity}`, { headers: { 'Authorization': `Bearer ${json.value}` } });
+        const json2 = await res2.json();
 
-    if (!json2.token) { console.log(`::error::${json2.message}`); process.exit(1); }
-    const tok = json2.token;
-    console.log(`::add-mask::${tok}`);
-    const fs = require('fs');
-    fs.appendFile(process.env.GITHUB_OUTPUT, `token=${tok}`, function (err) { if (err) throw err; }); // Write the output.
-    fs.appendFile(process.env.GITHUB_STATE, `token=${tok}`, function (err) { if (err) throw err; }); // Write the state, so the post job can delete the token.
-} catch (err) {
-    console.log(`::error::${err.stack}`); process.exit(1);
-}
+        if (!json2.token) { console.log(`::error::${json2.message}`); process.exit(1); }
+        const tok = json2.token;
+        console.log(`::add-mask::${tok}`);
+        const fs = require('fs');
+        fs.appendFile(process.env.GITHUB_OUTPUT, `token=${tok}`, function (err) { if (err) throw err; }); // Write the output.
+        fs.appendFile(process.env.GITHUB_STATE, `token=${tok}`, function (err) { if (err) throw err; }); // Write the state, so the post job can delete the token.
+    } catch (err) {
+        console.log(`::error::${err.stack}`); process.exit(1);
+    }
+})();
--- post.js
+++ post.js
@@ -5,17 +5,20 @@ if (!tok) {
     process.exit(0);
 }
 
-try {
-    const res = await fetch('https://api.github.com/installation/token', {
-        method: 'DELETE',
-        headers: { 'Authorization': `Bearer ${tok}` },
-    });
+(async function main() {
+    try {
+        const res = await fetch('https://api.github.com/installation/token', {
+            method: 'DELETE',
+            headers: { 'Authorization': `Bearer ${tok}` },
+        });
 
-    if (res.status == 204) {
-        console.log('::warning::Token was revoked!');
-    } else {
-        console.log(`::error::${res.status} ${res.statusText}`);
+        if (res.status == 204) {
+            console.log('::warning::Token was revoked!');
+        } else {
+            console.log(`::error::${res.status} ${res.statusText}`);
+        }
+    } catch (err) {
+        console.log(`::error::${err.stack}`);
     }
-} catch (err) {
-    console.log(`::error::${err.stack}`);
-}
+
+})();
