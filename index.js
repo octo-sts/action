e4d8e9326d6841b2e362853e12ff2028f4a84a5b
Jason Hall <jason@chainguard.dev>
2024-01-28 10:19:42 -0500

    async wtf
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ fetch(`${actionsUrl}?audience=octo-sts.dev`, { headers: { 'Authorization': `Bear
     .then(json => {
         const ghtok = json.token;
         fetch(`https://octo-sts.dev/sts/exchange?scope=${scope}&identity=${identity}`, { headers: { 'Authorization': `Bearer ${ghtok}` } })
-            .then(res => res.json())
-            .catch(err => { console.log(`::error::${err}`); process.exit(1); })
-            .then(json => {
-                if (!json.token) {
-                    console.log(`::error::${json.message}`);
-                    process.exit(1);
-                }
-                const tok = json.token;
-                console.log(`::add-mask::${tok}`);
-                process.env.GITHUB_OUTPUT += `token=${tok}`;
-            })
+            .then(res => res.json()
+                .catch(err => { console.log(`::error::${err}`); process.exit(1); })
+                .then(json => {
+                    if (!json.token) {
+                        console.log(`::error::${json.message}`);
+                        process.exit(1);
+                    }
+                    const tok = json.token;
+                    console.log(`::add-mask::${tok}`);
+                    process.env.GITHUB_OUTPUT += `token=${tok}`;
+                })
+            )
             .catch(err => { console.log(`::error::${err}`); process.exit(1); });
     })
     .catch(err => { console.log(`::error::${err}`); process.exit(1); });
